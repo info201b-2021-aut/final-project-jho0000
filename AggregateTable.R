36751c7d8222d8465ead173e07877684f3c0e6b6
DevinaT <91906378+DevinaT@users.noreply.github.com>
2021-11-19 23:50:20 -0800

    Add files via upload

---
+++ AggregateTable.R
@@ -0,0 +1,9 @@
+Average_Victim_Age <- "13"
+Amount_of_Cities_With_The_Smallest_Number_of_Homicides <- "53"
+Largest_Number_of_Homicides_in_a_Single_State <- "8100400"
+Number_of_Columns <- "24"
+Number_of_Rows <- "638454"
+Kaggle <- data.frame(Average_Victim_Age, Amount_of_Cities_With_The_Smallest_Number_of_Homicides, Largest_Number_of_Homicides_in_a_Single_State, Number_of_Columns, Number_of_Rows)
+Kaggle
+knitr::kable(Kaggle, "pipe", col.names = c("Average_Victim_Age", "Amount_of_Cities_With_The_Smallest_Number_of_Homicides", "Largest_Number_of_Homicides_in_a_Single_State", "Number_of_Columns", "Number_of_Rows"), align = c("l", "c", "c", "c", "r"))
+View(Kaggle)
