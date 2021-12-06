2146a3fb01f82bb0e30d705fac88c747b3087a7f
DevinaT <devinat@uw.edu>
2021-12-06 09:30:46 -0800

    done?

--- app.r
+++ app.r
@@ -3,7 +3,17 @@ library(shiny)
 library(fmsb)
 library(plotly)
 
-KaggleData <- read.csv("C:/Users/lucyl/OneDrive/Desktop/final-project-jho0000/data/KaggleData.csv")
+KaggleData <- read.csv("C:/Users/devin/Documents/201Projects/final-project-jho0000/data/KaggleData.csv")
+
+#This is for Devina's extra map
+Kaggle <- filter(KaggleData, State == "Washington")
+Kaggle <- filter(Kaggle, Year == 2014)
+register_google(key = "AIzaSyCFvgKY8R37iTAFKAJlmk8S6OZITaD4BKg")
+df <- select(Kaggle, City, State)
+df <- cbind(Kaggle, geocode(paste(df$City, df$State)))
+Kaggle <- merge(Kaggle, df)
+
+
 
 intro_page <- tabPanel(
   titlePanel("examining homocide"),
@@ -41,16 +51,37 @@ victim_age_scatter <- tabPanel(
   )
 )
 
-thefuckingMap <- 
+theMap <- tabPanel(
+  titlePanel("Murder Locations in Washington (2014)"),
+  
+  # Sidebar with a selectInput for the variable for analysis
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "analysis_var",
+        label = "Level of Analysis",
+        choices = c("Victim.Sex", "Perpetrator.Sex", "Victim.Race", "Perpetrator.Race")
+      )
+    ),
+    
+    # Display the map and table in the main panel
+    mainPanel(
+      leafletOutput("murder_map") # reactive output provided by leaflet
+    )
+  )
+)
 
 ui <- navbarPage(
   titlePanel("The Project"),
   intro_page,
-  victim_age_scatter
+  victim_age_scatter,
+  theMap
   
 )
 
 server <- function(input, output, session) {
+  
+  #This is Lucy's code------------------------------------------------------------------------
 
   Scatter_byState <- function(input_Year){
      Incidents_df <- select(KaggleData, Year, State, Incident, Victim.Age) %>% 
@@ -67,7 +98,35 @@ server <- function(input, output, session) {
       layout(title = 'Number of Homocides based on Victim Age in selected year in Washignton State', xaxis = list(title = 'Victim Age'), 
              yaxis = list(title = 'Number of Homocide Incidents'), width = 1100, height = 750)
 
-  })  
+  })
+  
+  #this is Devina's sexy code -------------------------------------------------------------
+  # Define a map to render in the UI
+  output$murder_map <- renderLeaflet({
+    
+    # Construct a color palette (scale) based on chosen analysis variable
+    palette_fn <- colorFactor(palette = "Dark2", domain = Kaggle[[input$analysis_var]])
+    leaflet(data = Kaggle) %>%
+      addProviderTiles("Stamen.TonerLite") %>% # add Stamen Map Tiles
+      addCircleMarkers( # add markers for each murder
+        lng = ~ lon,
+        lat= ~ lat,
+        label = ~paste0(Month, ", ", Weapon), # add a hover label: victim's name and age
+        color = ~palette_fn(Kaggle[[input$analysis_var]]), # color points by race
+        fillOpacity = .7,
+        radius = 4,
+        stroke = FALSE
+      ) %>%
+      addLegend( # include a legend on the plot
+        position = "bottomright",
+        title = "Race",
+        pal = palette_fn, # the palette to label
+        values = Kaggle[[input$analysis_var]], # again, using double-bracket notation
+        opacity = 1 # legend is opaque
+      )
+    
+  })
+  
 }
 
 shinyApp(ui, server)
